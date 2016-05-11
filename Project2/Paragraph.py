e4e85dd69c55325e4d271ab421384dbd57c7bf03
zhengqixi <zhengqixi@github.com>
2016-05-10 20:12:43 -0400

    changes to Paragraph.py for real this time

--- Project2/Paragraph.py
+++ Project2/Paragraph.py
@@ -3,14 +3,29 @@ from __future__ import print_function, division
 class Paragraph:
     def __init__(self):
         self.first = "" # first sentence (String)
-        self.sentences = list() # scrambled sentences (list of String)
-        self.order = list() # correct ordering of sentences (list of Int)
+        self.scrambled_sentences = list() # scrambled sentences (list of String)
+        self.correct_order = list() # correct ordering of sentences (list of Int)
+        self.predicted_order = list()
+        self.ordered_sentences = list()
+        
     def set_first(self, first): # save first sentence
         self.first = first
-    def set_order(self, order): # set correct ordering, stripping newline
-        self.order = list(order.strip('\n'))
+
+    def set_correct_order(self, order): # set correct ordering, stripping newline
+        self.correct_order = list(order.strip('\n'))
+
+    def set_predicted_order(self, order):
+        self.predicted_order = list(order,strip('\n'))
+
     def add_sentence(self, sen): # add sentence to list, stripping newline and letter designation
-        self.sentences.append(sen.strip('\n')[3:])
+        self.scrambled_sentences.append(sen.strip('\n')[3:])
+        
+    def order_sentence(self):
+        if not self.predicted_order or not self.scrambled_sentences:
+                print('Please set predicted order and scrambled sentences')
+                return False
+        self.ordered_sentences = [sentence for order, sentence in sorted(zip(self.correct_order, self.scrambled_sentences))]
+
     def __str__(self):
         sentences = ""
         order = ""
@@ -19,11 +34,14 @@ class Paragraph:
         for i in range(len(self.order)):
             order += self.order[i]
         return '\n' + self.first + '\n' + sentences + order + '\n'
+
     def __repr__(self):
         sentences = ""
         order = ""
+        correct_sentences = ""
         for i in range(len(self.sentences)):
-            sentences += self.sentences[i] + '\n'
+            sentences += self.scrambled_sentences[i] + '\n'
+            correct_sentences += self.ordered_sentences[i] + '\n'
         for i in range(len(self.order)):
-            order += self.order[i]
-        return '\n' + self.first + '\n' + sentences + order + '\n'
+            order += self.correct_order[i]
+        return '\n' + self.first + '\n' + sentences + '\n' + correct_sentences +  order + '\n'
