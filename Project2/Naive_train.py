12ba8643564a98c23f384c0f5cb1a1430a1fb3c0
zhengqixi <zhengqixi@github.com>
2016-05-11 00:01:30 -0400

    Naive Bayes model added

---
+++ Project2/Naive_train.py
@@ -0,0 +1,11 @@
+from nltk import word_tokenize
+import Paragraph
+
+class Naive_Bayes:
+
+    def __init__(self, n):
+       self.token_probability= list()
+       self.tokens = list()
+       for i in range(0, n):
+           tokens.append(list())
+
